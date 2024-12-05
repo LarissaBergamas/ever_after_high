813b7b729d5cad75c91419c0f6f4837213fb30b4
LarissaBergamas <larissabergamas@aluno.ifsp.edu.br>
2024-12-05 08:37:05 -0300

    lógica pronta

---
+++ script.js
@@ -0,0 +1,60 @@
+function testar(){
+
+    let contMadeline=0;
+    let contBriar=0;
+    let contApple=0;
+    let contRaven=0;
+
+    let radios = document.querySelectorAll('input[type="radio"]')
+    console.log(radios)
+
+    radios.forEach(e =>{
+        if(e.checked){
+
+            if(e.value == "madeline"){
+                contMadeline++;
+            } else if(e.value == "briar"){
+                contBriar ++;
+            } else if(e.value == "apple"){
+                contApple ++;
+            }else if(e.value == "raven"){
+                contRaven ++;
+            }
+            
+        } 
+    })
+
+
+    let resultado = `Madeline = ${contMadeline}<br>
+    Briar = ${contBriar}<br> Apple = ${contApple}<br>
+    Raven = ${contRaven}<br>`;
+
+    document.getElementById("resultado").innerHTML = resultado;
+
+    localStorage.setItem('madeline', contMadeline)
+    localStorage.setItem('briar', contBriar)
+    localStorage.setItem('apple', contApple)
+    localStorage.setItem('raven', contRaven)
+
+
+    let maior = 0
+    let pagina
+    if (contMadeline > maior) {
+        pagina = 'madeline'
+    }
+    if (contBriar > maior) {
+        pagina = 'briar'
+    }
+    if (contApple > maior) {
+        pagina = 'apple'
+    }
+    if (contRaven > maior) {
+        pagina = 'raven'
+    }
+
+    setTimeout(() => {
+        window.open(pagina+'.html')
+    }, 5000);
+
+
+}
